77b63ea8bafd7e72f881ca83ec7609409f686bce
JensenBromm <jensenbromm580@gmail.com>
2023-03-28 08:33:21 -0400

    Inital Commit
    Still need save to file button and view contacts button to work
    Also need a screen after submission

---
+++ saveFile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            if(isset($_POST['submit']))
+            {
+                $file=fopen("savedInfo.txt","a");
+
+                $firstName=$_POST['firstName'];
+                fwrite($file,"First Name: ");
+                fwrite($file,$firstName ."\n");
+                $lastName=$_POST['lastName'];
+                fwrite($file,"Last Name: ");
+                fwrite($file,$lastName ."\n");
+                $address=$_POST['address'];
+                fwrite($file,"Address: ");
+                fwrite($file,$address ."\n");
+                $state=$_POST['state-choice'];
+                fwrite($file,"State: ");
+                fwrite($file,$state ."\n");
+                $zip=$_POST['zip'];
+                fwrite($file,"Zip Code: ");
+                fwrite($file,$zip."\n");
+                $phone=$_POST['phone'];
+                fwrite($file,"Phone Number: ");
+                fwrite($file,$phone ."\n");
+                $email=$_POST['email'];
+                fwrite($file,"E-Mail: ");
+                fwrite($file,$email ."\n");
+
+            }
+        ?>
+
+    </body>
+</html>
